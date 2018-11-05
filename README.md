219583be85706cf6492d2a44901d6e4ce4706e1e
Philip Guo <philip@pgbovine.net>
2018-11-05 08:02:51 -0800

    ported over from https://github.com/pgbovine/lab4

---
+++ README.md
@@ -0,0 +1,8 @@
+lab4
+====
+
+Simulating a backend without needing a server. Created by Philip Guo in
+Nov 2017.
+
+See [pre-2017-version/](pre-2017-version/) for how the lab was before
+Philip Guo simplified it in Fall 2017.
---
+++ js/lab4.js
@@ -0,0 +1,54 @@
+// This serves as a fake database:
+var simpleData = {'title': 'Waiting in Line',
+                  'profilePic': 'images/lorempixel.people.1.jpeg',
+                  'index': 1};
+
+var complexData = [
+  {'title': 'Needfinding', 'profilePic': 'images/lorempixel.city.1.jpeg', 'index': 2},
+  {'title': 'Prototyping', 'profilePic': 'images/lorempixel.technics.1.jpeg', 'index': 3},
+  {'title': 'Heuristic Evaluation', 'profilePic': 'images/lorempixel.abstract.1.jpeg', 'index': 4},
+  {'title': 'Visualization', 'profilePic': 'images/lorempixel.abstract.8.jpeg', 'index': 5},
+  {'title': 'Social design', 'profilePic': 'images/lorempixel.people.2.jpeg', 'index': 6},
+  {'title': 'Gestural interaction', 'profilePic': 'images/lorempixel.technics.2.jpeg', 'index': 7},
+  {'title': 'Design tools', 'profilePic': 'images/lorempixel.city.2.jpeg', 'index': 8}
+]
+
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+  console.log('hello world');
+
+  // compile the template
+  var source   = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+
+  var parentDiv = $("#templatedProjects");
+
+  // start with a simple template
+  var html = template(simpleData);
+  console.log(html);
+  parentDiv.append(html);
+
+  // now iterate through the complexData list and keep appending:
+  for (var i = 0; i < complexData.length; i++) {
+    var curData = complexData[i];
+    var curHtml = template(curData);
+    parentDiv.append(curHtml);
+  }
+
+
+  // when you first load the page, set a custom name if you have one:
+  var name = localStorage.getItem('customName');
+  if (name) {
+    $("#myName").html(name);
+  }
+
+  // use localStorage to store your name
+  $("#changeName").click(function() {
+    var newName = prompt("What's your new name?");
+    if (newName) {
+      $("#myName").html(newName);
+      localStorage.setItem('customName', newName);
+    }
+  });
+})
